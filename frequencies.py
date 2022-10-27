17eef5ee1a373a47b804cb6fa9738cb8ae64dc89
Allister Gough <allistergough02@gmail.com>
2022-10-27 13:38:50 +0100

    Finish solution

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,18 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from xml.etree.ElementTree import tostring
+
+
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    for x in range(len(items)):
+        items[x] = str(items[x])
+
+    for i in range(len(items)):
+        element = items[i]
+        frequencies[element] = items.count(element)
+
     return frequencies
